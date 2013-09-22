7aec065d6d5374f1957e287850dddedfdb4e3837
alexandermitchell <alex.mitchell@mac.com>
2013-09-22 16:12:21 -0400

    Created working maximum capacity method

--- SeatYourself/app/models/reservation.rb
+++ SeatYourself/app/models/reservation.rb
@@ -3,6 +3,13 @@ class Reservation < ActiveRecord::Base
 	belongs_to :customer
 	belongs_to :restaurant
 
-	
+	validate :less_than_max_occupancy
 
+	def less_than_max_occupancy
+	  other_people = Reservation.where(:restaurant_id => self.restaurant_id, :time_slot => self.time_slot.day).sum(:groupsize)
+
+	  if other_people + self.groupsize > 10
+	     errors.add(:base, "Sorry, too many people!")
+	  end
+	end
 end
